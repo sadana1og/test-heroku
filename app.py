cc94dacb7a86e86d71d55c30274344697e23fb9e
sadanalog <sadanalog@gmail.com>
2020-03-17 21:57:56 +0700

    test2

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route('/input', methods=["POST"])
 def input():
     payload = request.get_json(force=True)
-    x = payload['x']
-    y = payload['y']
+    x = int(payload['x'])
+    y = int(payload['y'])
     if x < 0 or x > ROOM_SIZE_X or y < 0 or y > ROOM_SIZE_Y :
         answer = 'out'
     else :
