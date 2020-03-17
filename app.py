e519e75e0b764099df2621d310b7caaa0f02b006
sadanalog <sadanalog@gmail.com>
2020-03-17 21:55:27 +0700

    test1

--- app.py
+++ app.py
@@ -1,51 +1,19 @@
-# app.py
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
+@app.route('/input', methods=["POST"])
+def input():
+    payload = request.get_json(force=True)
+    x = payload['x']
+    y = payload['y']
+    if x < 0 or x > ROOM_SIZE_X or y < 0 or y > ROOM_SIZE_Y :
+        answer = 'out'
+    else :
+        answer = 'in'
+    resp = {'status': answer, 'x': x, 'y': x}
+    return jsonify(resp)
+    
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(debug=True, port=8000)
